23a17cd1ca32a7fc2b05af175105aaa6e48a93dc
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2020-06-08 17:44:11 +0200

    Remove unused constant

--- src/Normalizer/AbsoluteDateNormalizer.php
+++ src/Normalizer/AbsoluteDateNormalizer.php
@@ -59,11 +59,6 @@ class AbsoluteDateNormalizer implements NormalizerInterface, DenormalizerInterfa
         return $data instanceof AbsoluteDate;
     }
 
-    public const EMPTY_STRING_OR_NULL_EXCEPTION_MESSAGE = <<<EOT
-The data is either an empty string or null.
-You should pass a string that can be parsed with the passed format or a valid Y-m-d string.
-EOT;
-
     /**
      * {@inheritdoc}
      *
