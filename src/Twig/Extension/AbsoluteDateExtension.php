992bec7d555d7e32842cb5f4b147d54e808af00a
Guillaume Wambre <guillaume.wambre@assoconnect.com>
2022-12-08 15:50:29 +0100

    Update AbsoluteDateExtension.php

--- src/Twig/Extension/AbsoluteDateExtension.php
+++ src/Twig/Extension/AbsoluteDateExtension.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Twig;
+namespace App\Twig\Extension;
 
 use AssoConnect\PHPDate\AbsoluteDate;
 use AssoConnect\PHPDateBundle\Translatable\AbsoluteDateTranslatable;
